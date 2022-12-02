ccef0f0bf58a816a83015ab4009f61e4abe5e495
filipSlesicki <fslesicki@gmail.com>
2022-12-02 11:46:29 +0100

    Inventory item description

--- Programowanie 3/Assets/Scripts/3Dtest/Inventory.cs
+++ Programowanie 3/Assets/Scripts/3Dtest/Inventory.cs
@@ -7,7 +7,9 @@ public class Inventory : MonoBehaviour
 {
     public List<Item> items;
     [SerializeField] GameObject inventoryWindow;
-    
+    [SerializeField] InventoryItem inventoryItemPrefab;
+
+    List<GameObject> createdIcons = new List<GameObject>();
 
     public void OpenInventory(InputAction.CallbackContext context)
     {
@@ -15,6 +17,12 @@ public class Inventory : MonoBehaviour
         {
             inventoryWindow.SetActive(true);
             UIManager.instance.SwitchToUIInput();
+            foreach (var item in items)
+            {
+                InventoryItem inventoryItem = Instantiate(inventoryItemPrefab, inventoryWindow.transform);
+                inventoryItem.Setup(item);
+                createdIcons.Add(inventoryItem.gameObject);
+            }
         }
     }
     public void CloseInventory(InputAction.CallbackContext context)
@@ -23,6 +31,11 @@ public class Inventory : MonoBehaviour
         {
             inventoryWindow.SetActive(false);
             UIManager.instance.SwitchToGamplayInput();
+            foreach (var icon in createdIcons)
+            {
+                Destroy(icon);
+            }
+            createdIcons.Clear();
         }
     }
 
--- Programowanie 3/Assets/Scripts/3Dtest/InventoryItem.cs
+++ Programowanie 3/Assets/Scripts/3Dtest/InventoryItem.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.EventSystems;
 
-public class InventoryItem : MonoBehaviour
+public class InventoryItem : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Image image;
+    [SerializeField] TMP_Text itemName;
+    [SerializeField] TMP_Text itemDescription;
+    [SerializeField] GameObject descriptionWindow;
+
+    public void Setup(Item item)
     {
-        
+        image.sprite = item.icon;
+        itemName.text = item.itemName;
+        itemDescription.text = item.description;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ShowDescription(bool show)
     {
-        
+        descriptionWindow.SetActive(show);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        //ShowDescription(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        //ShowDescription(false);
     }
 }
